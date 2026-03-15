57e352f8c6681cf022fb51816b7bf30660608b83
aiko <aiko@ludomedia.ch>
2026-03-15 10:37:31 +0100

    derver

--- config/nvim/lua/plugins.lua
+++
@@ -1,9 +0,0 @@
-vim.cmd [[
-call plug#begin('~/.config/nvim/plugged')
-
-Plug 'sheerun/vim-polyglot'     " Coloration multi-langages
-Plug 'joshdick/onedark.vim'     " Thème Onedark
-Plug 'dense-analysis/ale'       " Linter en direct
-
-call plug#end()
-]]
